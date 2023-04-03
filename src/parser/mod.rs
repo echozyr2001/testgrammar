3ae24fbfcda42573f054ffb2b8550a0110b87d8c
YaoerWu <76507621+YaoerWu@users.noreply.github.com>
2023-04-03 17:48:40 +0800

    remove test in mods

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -10,19 +10,3 @@ const DATA_PATH: &str = "./data/";
 const ACTION_TABLE: &str = "./data/action_table.rcp";
 const GOTO_TABLE: &str = "./data/goto_table.rcp";
 const LR1_SETS: &str = "./data/lr1_sets.rcp";
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let left = crate::parser::Element::NotTerminal("'{'".to_string());
-        let right = crate::parser::Element::NotTerminal("'}'".to_string());
-        assert!(left.is_paired(&right));
-        let left = crate::parser::Element::NotTerminal("'('".to_string());
-        let right = crate::parser::Element::NotTerminal("')'".to_string());
-        assert!(left.is_paired(&right));
-        let left = crate::parser::Element::NotTerminal("'['".to_string());
-        let right = crate::parser::Element::NotTerminal("']'".to_string());
-        assert!(left.is_paired(&right));
-    }
-}
